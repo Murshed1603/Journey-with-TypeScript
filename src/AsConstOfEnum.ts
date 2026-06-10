c8c998de5ef47fdaadc3472f196d84556c7e6924
Murshed Ahmed <murshedahmedibrahim2001@gmail.com>
2026-06-10 23:55:41 +0600

    modified

--- src/AsConstOfEnum.ts
+++ src/AsConstOfEnum.ts
@@ -10,14 +10,28 @@
 
 const UserRoles = {
     Admin : "Admin",
-    Editor: "Editor",
+    Editor: "Editor", // javascript e object er bodole colon use korte hobe
     Viewer: "Viewer",
-};
+} as const;
 
-UserRoles.Admin = 'Mon chacche';
+// UserRoles.Admin = 'Mon chacche';
+
+// 1. typeof operator
+// 2. keyof operator
+
+// const user = {
+//     id: 22,
+//     name: 'Mezba'
+// }
+// typeof user; // eta likhle nicher ta hobe ==>
+
+// type user = {
+//     id: number,
+//     name: string,
+// };
 
 
-const canEdit = (role: UserRoles) => {
+const canEdit = (role: keyof typeof UserRoles) => {
     if (role === UserRoles.Admin || role === UserRoles.Editor) {
         return true;
     } else {
