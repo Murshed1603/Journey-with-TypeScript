174e6dffdee962e6243f1070ceade5ce7a94c271
Murshed Ahmed <murshedahmedibrahim2001@gmail.com>
2026-06-12 15:03:37 +0600

    Fixed Mapped Types

--- src/mappedType.ts
+++ src/mappedType.ts
@@ -10,3 +10,37 @@ const arrayOfStringUsingMap : string[] =
 arrayOfNum.map((num) => num.toString());
 
 console.log(arrayOfStringUsingMap);
+
+const user = {
+    id: 222,
+};
+
+
+
+
+type AreaOfNum = { // ekhane AreaOfNum ekta OBJECT  
+    height: number;
+    width: number;
+};
+
+type height = AreaOfNum["height"];
+
+// type AreaOfString = {
+//     height: string;
+//     width: string;
+// };
+
+type Area<T> = {
+    [key in keyof T] : T[key];
+};
+
+/*
+T => {height: string; width: string }
+*/
+
+const area1 : Area<{height: string; width:
+boolean }> = { 
+    height: '50',
+    width: false,
+
+};
